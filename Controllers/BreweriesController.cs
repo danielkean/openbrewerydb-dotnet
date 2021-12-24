37f38d01b53040a94f40f6b1c0c0706df52e5027
Daniel Kean <64756591+danielkean@users.noreply.github.com>
2021-12-24 10:43:38 +0000

    Add summaries for each of the 'BreweryController' methods
    Define the summary, parameter definitions, and what's being returned for each endpoint method.

--- Controllers/BreweriesController.cs
+++ Controllers/BreweriesController.cs
@@ -9,6 +9,10 @@ public class BreweriesController : ControllerBase
     private readonly HttpClient client = new();
     private const string BASE_URL = "https://api.openbrewerydb.org/breweries/";
 
+    /// <summary>
+    /// Get a list of breweries through a collection of optional parameters.
+    /// </summary>
+    /// <returns>A list of breweries that match the parameters. If none are provided, return all breweries.</returns>
     [HttpGet]
     public async Task<ActionResult<List<Brewery>>> ListBreweries()
     {
@@ -16,6 +20,11 @@ public class BreweriesController : ControllerBase
         return breweries == null ? NotFound() : breweries;
     }
 
+    /// <summary>
+    /// Get a brewery by id.
+    /// </summary>
+    /// <param name="id">The id of the brewery that should be returned.</param>
+    /// <returns>The brewery that matches the 'id' if one exists.</returns>
     [HttpGet("{id}")]
     public async Task<ActionResult<Brewery>> GetBrewery(string id)
     {
@@ -23,6 +32,11 @@ public class BreweriesController : ControllerBase
         return brewery == null ? NotFound() : brewery;
     }
 
+    /// <summary>
+    /// Search breweries using a query.
+    /// </summary>
+    /// <param name="query">The query to use for searching the breweries.</param>
+    /// <returns>List of breweries that match the search query.</returns>
     [HttpGet("search")]
     public async Task<ActionResult<List<Brewery>>> SearchBreweries(string query)
     {
