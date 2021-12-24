6ab7645e20ba85951d64142517c02dde28820f68
Daniel Kean <64756591+danielkean@users.noreply.github.com>
2021-12-24 10:14:06 +0000

    Update Brewery model so JSON properties that are two words use snake casing
    As 'Open Brewery DB' uses snake casing for its JSON properties, 'JsonPropertyName' attributes need to be added to properties that are two words, otherwise they will be represented with camel casing.

--- Models/Brewery.cs
+++ Models/Brewery.cs
@@ -1,22 +1,37 @@
+using System.Text.Json.Serialization;
+
 namespace OpenBrewery;
 
 public class Brewery
 {
+    /*
+        NOTE: Properties with two words have custom JSON property names
+        as to match the 'snake_case' format that 'OpenBreweryDB' uses.
+    */
+
     public string? Id { get; set; }
     public string Name { get; set; } = "";
+    [JsonPropertyName("brewery_type")]
     public string BreweryType { get; set; } = "";
     public string Street { get; set; } = "";
+    [JsonPropertyName("address_2")]
     public string? Address2 { get; set; }
+    [JsonPropertyName("address_3")]
     public string? Address3 { get; set; }
     public string City { get; set; } = "";
     public string State { get; set; } = "";
+    [JsonPropertyName("county_province")]
     public string CountyProvince { get; set; } = "";
+    [JsonPropertyName("postal_code")]
     public string PostalCode { get; set; } = "";
     public string Country { get; set; } = "";
     public string? Longitude { get; set; }
     public string? Latitude { get; set; }
     public string? Phone { get; set; }
+    [JsonPropertyName("website_url")]
     public string? WebsiteUrl { get; set; }
+    [JsonPropertyName("updated_at")]
     public string? UpdatedAt { get; set; }
+    [JsonPropertyName("created_at")]
     public string? CreatedAt { get; set; }
 }
